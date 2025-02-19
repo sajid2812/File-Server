9ad1c3200f55f6efad3eed1d7ad09691b7692497
sajid-edninja <ssk73573@gmail.com>
2025-02-19 08:52:46 +0530

    add files section

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ const app = express();
 app.get("/", (req, res) => {
   return res.status(200).end("<h1>Welcome to the File Server</h1>");
 });
+
+app.get("/files", (req, res) => {
+  const dirPath = path.join(__dirname, "/files");
+  fs.readdir(dirPath, (err, files) => {
+    if (err) {
+      return res.status(404).json({ message: "Directory not found." });
+    }
+    return res.status(200).json(files);
+  });
+});
